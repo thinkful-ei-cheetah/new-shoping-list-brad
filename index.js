da7329a434c94431b4acb1becf437d5a10be9675
Peter Pae <paendabear@gmail.com>
2019-03-29 12:45:49 -0400

    add delete

--- index.js
+++ index.js
@@ -38,11 +38,84 @@ function generateShoppingItemHtml(item, itemIndex){
   }
 
 const SHOPPING_LIST_EL = $('.shopping-list');
+// turn css class into variable
 
 function renderShoppingList() {
   const html = generateShoppingListElements(STORE);
+  // html = list elements HTML
   SHOPPING_LIST_EL.html(html);  
+  // css class html is now list elements html
 }
 
   
   console.log(generateShoppingListElements(STORE));
+
+  function addItemToShoppingList(itemName) {
+    // function pushes obj to array with name and checked keys
+    console.log(`Adding "${itemName}" to shopping list`);
+    STORE.push({id: cuid(), name: itemName, checked: false});
+  }
+
+
+  function handleNewItemSubmit() {
+    // when new submit reset dom usage
+    $('#js-shopping-list-form').submit(function(event) {
+      event.preventDefault();
+      const newItemName = $('.js-shopping-list-entry').val();
+      
+      
+      console.log('`handleNewItemSubmit` ran');
+      $('.js-shopping-list-entry').val('');
+      // resets value of form to empty string
+
+      addItemToShoppingList(newItemName);
+    });
+  }
+
+  function toggleCheckedForListItem(itemId) {
+    console.log("Toggling checked property for item with id " + itemId);
+    const item = STORE.find(item => item.id === itemId);
+    item.checked = !item.checked;
+  }
+  
+  
+  function getItemIdFromElement(item) {
+    return $(item)
+      .closest('li')
+      .data('item-id');
+  }
+  
+  function handleItemCheckClicked() {
+    $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+      console.log('`handleItemCheckClicked` ran');
+      const id = getItemIdFromElement(event.currentTarget);
+      toggleCheckedForListItem(id);
+      renderShoppingList();
+    });
+  }
+
+  function deleteListItem(itemId) {
+    console.log(`Deleting item with id  ${itemId} from shopping list`)
+  
+    // as with `addItemToShoppingLIst`, this function also has the side effect of
+    // mutating the global STORE value.
+    //
+    // First we find the index of the item with the specified id using the native
+    // Array.prototype.findIndex() method. Then we call `.splice` at the index of 
+    // the list item we want to remove, with a removeCount of 1.
+    const itemIndex = STORE.findIndex(item => item.id === itemId);
+    STORE.splice(itemIndex, 1);
+  }
+  
+  
+  function handleDeleteItemClicked() {
+    // like in `handleItemCheckClicked`, we use event delegation
+    $('.js-shopping-list').on('click', '.js-item-delete', event => {
+      // get the index of the item in STORE
+      const itemIndex = getItemIdFromElement(event.currentTarget);
+      // delete the item
+      deleteListItem(itemIndex);
+      // render the updated shopping list
+      renderShoppingList();
+    });
+  }
